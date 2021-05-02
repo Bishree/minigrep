95435e3aa220d84de17e50cd18f917c575e4fbd7
Bishree <ahmedbishree@gmail.com@users.noreply.github.com>
2021-05-03 00:11:14 +0300

    _12.3_split_the_code_to_lib

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,25 @@
 use std::env;
-use std::fs;
+use std::process;
+
+use minigrep::Config;
 
 fn main() {
+    // --snip--
     let args: Vec<String> = env::args().collect();
-    let config = Config::new(&args);
 
-    println!("{:?}", args);
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
 
-    println!("Searching for  {}", config.query);
+    println!("Searching for {}", config.query);
     println!("In file {}", config.filename);
 
-    let contents = fs::read_to_string(filename)
-        .expect("something went wrong reading the file");
-    println!("with text:\n{}", contents);
+    if let Err(e) = minigrep::run(config) {
+        // --snip--
+        println!("Application error: {}", e);
 
-struct Config {
-    query: String,
-    filename: String,
-}
-impl Config {
-    fn new (args:&[String]) -> Config {
-        let query = args [1].clone();
-        let filename = args [2].clone();
-
-        Config {query, filename}
+        process::exit(1);
     }
-}
-
-//
-// fn pars_config (args: &[String]) -> Config {
-//     let query = args[1].clone();
-//     let filename = args[2].clone();
-//
-//     Config {query, filename}
-// }
-
 
 }
